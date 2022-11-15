1c25abba8e797e3afd66cc4e57445686d064d0d7
jroberts9495 <114313865+jroberts9495@users.noreply.github.com>
2022-11-14 19:28:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# better-blind-clock
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-6tjhzx)
---
+++ src/app/blind-clock/blind-clock.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blind-clock',
+  templateUrl: './blind-clock.component.html',
+  styleUrls: ['./blind-clock.component.css'],
+})
+export class BlindClockComponent implements OnInit {
+  //@ViewChild('paused') paused: boolean = false;
+  readonly TIME_FORMAT = '{0}';
+  timerStart?: number = Date.now();
+  timeRemaining: string = '';
+
+  ngOnInit() {
+    setInterval(() => {
+      if (this.timerStart !== undefined) {
+        let elapsedNumeric = 60 - (Date.now() - this.timerStart) / 1000;
+        if (elapsedNumeric <= 0) {
+          this.timeRemaining = '00:00.0';
+        } else {
+          this.timeRemaining = `${this.pad(
+            Math.floor(elapsedNumeric / 60)
+          )}:${this.pad(elapsedNumeric % 60, 1)}`;
+        }
+      }
+    }, 100);
+  }
+
+  constructor() {}
+
+  private pad(input: number, numDecimals?: number): string {
+    if (numDecimals === undefined) {
+      numDecimals = 0;
+    }
+    if (input < 10) {
+      return `0${input.toFixed(numDecimals)}`;
+    } else {
+      return `${input.toFixed(numDecimals)}`;
+    }
+  }
+}
